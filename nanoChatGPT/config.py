4b97d05c3b14e34b746435f5407987570a385970
yoonhero <yoonhero06@naver.com>
2023-04-18 13:00:21 +0900

    fix

--- nanoChatGPT/config.py
+++ nanoChatGPT/config.py
@@ -34,4 +34,4 @@ KOGPT_CONFIG = GPTConfig(block_size=2048, n_embd=4096, n_heads=16, n_layer=28, v
 ULTRA_SUPER_SUPER_LARGE_LARGE_CHATGPT_CONFIG = GPTConfig(block_size=8192, n_embd=1024, n_heads=64, n_layer=12, dropout=0.2, vocab_size=100000)
 
 ## LLAMA 7B model configuration
-LLAMA_7B_CONFIG = GPTConfig(block_size=2048, n_embd=4096, n_heads=32, n_layer=32, vocab_size=32000)
+LLAMA_7B_CONFIG = GPTConfig(block_size=128, n_embd=384, n_heads=8, n_layer=10, vocab_size=64512)
