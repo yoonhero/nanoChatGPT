8c4f3073f5ca361d201fce43f278e9a8c3a99375
yoonhero <yoonhero06@naver.com>
2023-05-02 15:16:08 +0900

    feat: Few Traning Method Upgrade

--- train.py
+++ train.py
@@ -122,6 +122,7 @@ def main(args):
     
     train(
         model=model,
+        tokenizer=tokenizer,
         optimizer=optimizer, 
         train_loader=train_loader, 
         val_loader=val_loader, 
@@ -136,7 +137,7 @@ def main(args):
         is_wandb=is_wandb
     )    
 
-def train(model: torch.nn.Module, optimizer: torch.optim.Optimizer, train_loader, val_loader, output_dir: str, start_epoch: int, max_epoch: int, gradient_accumulation_interval: int, eval_interval: int, save_interval: int, learning_rate: float, with_lr_scheduler: bool, is_wandb: bool):   
+def train(model: torch.nn.Module, tokenizer: AutoTokenizer, optimizer: torch.optim.Optimizer, train_loader, val_loader, output_dir: str, start_epoch: int, max_epoch: int, gradient_accumulation_interval: int, eval_interval: int, save_interval: int, learning_rate: float, with_lr_scheduler: bool, is_wandb: bool):   
     scaler = torch.cuda.amp.GradScaler()
     losses = np.zeros(max_epoch)
 
